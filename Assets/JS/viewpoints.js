d135bc48596a14f39a950cc04f46581d55643050
Brock William <brock@Brocks-MacBook-Pro.local>
2023-09-08 05:58:03 +0200

    Made changes to js to implement google API features. I'm using example coords need to change to local storage feel free to make that change if you want I will work on it. Directions map should be displaying but isn't will have to fix later.

--- Assets/JS/viewpoints.js
+++ Assets/JS/viewpoints.js
@@ -1,75 +1,145 @@
-// Initialize variables
-const modal = document.getElementById("modal");
+
 const locationForm = document.getElementById("location-form");
-
-const carousel = document.getElementById("carousel");
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-const carouselImage = document.getElementById("carousel-image");
-const imageDescription = document.getElementById("image-description");
-
-const carousel2 = document.getElementById("carousel2");
-const prevBtn2 = document.getElementById("prev2");
-const nextBtn2 = document.getElementById("next2");
-const carouselImage2 = document.getElementById("carousel-image2");
-const imageDescription2 = document.getElementById("image-description2");
-
+const directionsSection = document.getElementById("directions-section");
 const directionsButton = document.getElementById("directions-button");
 const directionsButton2 = document.getElementById("directions-button2");
-const directionsSection = document.getElementById("directions-section");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const prevBtn2 = document.getElementById("prev2");
+const nextBtn2 = document.getElementById("next2");
 
-// Image URLs for the first carousel
-const imageUrls = [
-  'assets/images/IMG_2388.JPG',
-  'assets/images/DSC09685-HDR (1).jpg',
-  'assets/images/DSC02465-HDR (1).jpg'
-];
 
-// Image descriptions for the first carousel
-const imageDescriptions = [
-  'Description for image 1 in Attractions',
-  'Description for image 2 in Attractions',
-  'Description for image 3 in Attractions'
-];
+// Variables for Google Maps and Places API
+let currentImageIndex = 0; 
+let currentImageIndex2 = 0; 
+let map;
+let service;
+let infowindow;
+let directionsService;
+let directionsRenderer;
+let imagePlaceIds = [];
 
-// Image URLs for the second carousel
-const imageUrls2 = [
-  'assets/images/DSC09685-HDR (1).jpg',
-  'assets/images/DSC02465-HDR (1).jpg',
-  'assets/images/IMG_2388.JPG'
-];
 
-// Image descriptions for the second carousel
-const imageDescriptions2 = [
-  'Description for image 1 in Viewpoints',
-  'Description for image 2 in Viewpoints',
-  'Description for image 3 in Viewpoints'
-];
 
-let currentImageIndex = 0;
-let currentImageIndex2 = 0;
+// Adjusting the image arrays to be dynamic based on Google Maps results
+let imageUrls = [];
+let imageDescriptions = [];
 
-// Function to show the current image and description in the first carousel
+let imageUrls2 = [];
+let imageDescriptions2 = [];
+
+// Replace these coordinates with the user's coordinates
+let userLocation = { lat: 37.7749, lng: -122.4194 };  // Example: San Francisco
+
+// Initializes Google Maps and searches for nearby attractions
+function initMap() {
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.createElement("div"));
+  directionsService = new google.maps.DirectionsService();
+directionsRenderer = new google.maps.DirectionsRenderer();
+directionsRenderer.setMap(map);
+
+  // Searching for tourist attractions
+  let attractionsRequest = {
+      location: userLocation,
+      radius: '5000',   // Searching within a 5km radius
+      type: ['tourist_attraction']  
+  };
+
+  // Searching for viewpoints or hiking trails 
+  let viewpointsRequest = {
+      location: userLocation,
+      radius: '5000',   // Searching within a 5km radius
+      type: ['viewpoint', 'park']  
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(attractionsRequest, attractionsCallback);
+  service.nearbySearch(viewpointsRequest, viewpointsCallback);
+}
+function calculateAndDisplayRoute(startCoords, destinationPlaceId) {
+  directionsService.route(
+    {
+      origin: startCoords,
+      destination: { placeId: destinationPlaceId },
+      travelMode: 'DRIVING'
+    },
+    (response, status) => {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    }
+  );
+}
+function attractionsCallback(results, status) {
+if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        if (place.photos && place.photos.length > 0) {
+          imageUrls.push(place.photos[0].getUrl());
+          imageDescriptions.push(place.name);
+          imagePlaceIds.push(place.place_id);
+      }
+    }
+    showCurrentImage();
+}
+}
+
+function viewpointsCallback(results, status) {
+if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        if (place.photos && place.photos.length > 0) {
+            imageUrls2.push(place.photos[0].getUrl());
+            imageDescriptions2.push(place.name);
+            imagePlaceIds.push(place.place_id);
+        }
+    }
+    showCurrentImage2();
+}
+}
 function showCurrentImage() {
+  const carouselImage = document.getElementById("carousel-image");
+  const imageDescription = document.getElementById("image-description");
+
   carouselImage.src = imageUrls[currentImageIndex];
   imageDescription.textContent = imageDescriptions[currentImageIndex];
 }
 
-// Function to show the current image and description in the second carousel
 function showCurrentImage2() {
+  const carouselImage2 = document.getElementById("carousel-image2");
+  const imageDescription2 = document.getElementById("image-description2");
+
   carouselImage2.src = imageUrls2[currentImageIndex2];
   imageDescription2.textContent = imageDescriptions2[currentImageIndex2];
 }
 
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+          let place = results[i];
+          if (place.photos && place.photos.length > 0) {
+              imageUrls.push(place.photos[0].getUrl());
+              imageDescriptions.push(place.name);
+          }
+      }
+      showCurrentImage();
+  }
+}
+
+
+
 // Event Listener for form submission
 locationForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  modal.classList.add("hidden");
-  carousel.classList.remove("hidden");
-  carousel2.classList.remove("hidden");
-  showCurrentImage();
-  showCurrentImage2();
+    e.preventDefault();
+    modal.classList.add("hidden");
+    carousel.classList.remove("hidden");
+    carousel2.classList.remove("hidden");
+    initMap();   // Initialize Google Maps and fetch nearby attractions
 });
+
 
 // Event Listener for Previous Button in the first carousel
 prevBtn.addEventListener("click", () => {
@@ -95,11 +165,14 @@ nextBtn2.addEventListener("click", () => {
   showCurrentImage2();
 });
 
-// Show the directions section when the button is clicked
-directionsButton.addEventListener("click", () => {
+document.getElementById("directions-button").addEventListener("click", () => {
   directionsSection.classList.remove("hidden");
+  calculateAndDisplayRoute(userLocation, imagePlaceIds[currentImageIndex]);
+  google.maps.event.trigger(map, 'resize'); // add this line
 });
 
-directionsButton2.addEventListener("click", () => {
+document.getElementById("directions-button2").addEventListener("click", () => {
   directionsSection.classList.remove("hidden");
+  calculateAndDisplayRoute(userLocation, imagePlaceIds[currentImageIndex2]);
+  google.maps.event.trigger(map, 'resize'); // and this line
 });
