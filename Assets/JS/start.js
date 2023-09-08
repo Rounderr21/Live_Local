8a506c9e38c824d5bcd7bcca8ffdcd12a91e0a68
Chris <138630942+CrisCo116@users.noreply.github.com>
2023-09-07 20:00:29 -0700

    connected start page to home page

--- Assets/JS/start.js
+++ Assets/JS/start.js
@@ -1,24 +1,21 @@
 const locateBtn = document.querySelector('#startScrn');
 const titleEl = document.querySelector('.title')
 
-
-
 let latC;
 let lonC;
 
-locateBtn.addEventListener('click', function(){
-
+locateBtn.addEventListener('click', function() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-  window.location.assign(index.html);
-});
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latC = parseFloat(position.coords.latitude);
+      lonC = parseFloat(position.coords.longitude);
+      console.log(latC, lonC);
 
-function showPosition(position) {
- latC =  parseFloat(position.coords.latitude); 
- lonC = parseFloat(position.coords.longitude);
- 
- console.log(latC, lonC);
-};
+      // Now that you have the location, you can redirect to the next page.
+      window.location.href = "home-page.html";
+    });
+  } else {
+    // Handle the case where geolocation is not supported by the browser.
+    alert("Geolocation is not supported by this browser.");
+  }
+});
